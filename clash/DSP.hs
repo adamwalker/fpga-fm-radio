43cda379ac4e84df86520746a8cf158fbbf54425
Adam Walker <adamwalker10@gmail.com>
2022-07-23 19:10:10 +1000

    semi-parallel complex decimators

--- clash/DSP.hs
+++ clash/DSP.hs
@@ -13,6 +13,7 @@ import Clash.DSP.MAC
 import Clash.DSP.FIR.Filter
 import Clash.DSP.CORDIC
 import Clash.DSP.Fixed
+import Clash.DSP.FIR.HalfBand
 
 {-
  - from scipy import signal
@@ -29,6 +30,31 @@ coeffsHalfBand = $(listToVecTH [
         3.14152600e-01,  
         0.5 :: Double
     ])
+
+{-
+ - from scipy import signal
+ - signal.firwin(29, 0.5)
+ -}
+coeffsHalfBand2 :: Vec 32 (SFixed 1 17)
+coeffsHalfBand2 = $(listToVecTH $ Prelude.map (*2) [
+    -4.07163435e-04,  4.43611207e-04,
+    -5.06181165e-04,  5.97149189e-04,
+    -7.18882587e-04,  8.73864646e-04,
+    -1.06472738e-03,  1.29429470e-03,
+    -1.56563902e-03,  1.88215527e-03,
+    -2.24765779e-03,  2.66650781e-03,
+    -3.14378211e-03,  3.68549810e-03,
+    -4.29891790e-03,  4.99296405e-03,
+    -5.77879742e-03,  6.67063473e-03,
+    -7.68692977e-03,  8.85212126e-03,
+    -1.01992919e-02,  1.17743452e-02,
+    -1.36428182e-02,  1.59015000e-02,
+    -1.86993396e-02,  2.22776490e-02,
+    -2.70541552e-02,  3.38191101e-02,
+    -4.42675757e-02,  6.28091690e-02,
+    -1.05616750e-01,  3.18259237e-01 :: Double
+    ])
+
 
 -- | Real * Real multiply and accumulate with pre-add
 macPreAddRealReal' 
@@ -83,6 +109,32 @@ phaseDiff en x = x * xD'
     where
     xD' = delayEn undefined en $ conjugate <$> x
 
+decimateComplex2
+    :: (HiddenClockResetEnable dom, KnownNat coeffsPerStage, KnownNat numStages)
+    => 1 <= (coeffsPerStage + (numStages + (numStages * coeffsPerStage)))
+    => Vec (numStages + 1) (Vec (coeffsPerStage + 1) (SFixed 1 17))
+    -> Signal dom Bool
+    -> Signal dom (Complex (SFixed 1 23))
+    -> (
+        Signal dom Bool, 
+        Signal dom (Complex (SFixed 1 23)), 
+        Signal dom Bool
+        )
+decimateComplex2 coeffs valid sampleIn = (
+        validOut, 
+        fmap (truncateFrac . renorm) <$> sampleOut, 
+        ready
+    )
+    where
+  
+    (validOut, sampleOut, ready)  
+        = halfBandDecimate 
+            (coerce macPreAddRealComplexPipelined) 
+            (SNat @2) 
+            (fmap (resizeF :: SFixed 1 23 -> SFixed 5 40))
+            coeffs 
+            valid 
+            sampleIn
 fmRadio
     :: forall dom
     .  HiddenClockResetEnable dom 
@@ -95,24 +147,25 @@ fmRadio
 fmRadio en x = (en5, dat)
     where
 
+    sample0 = fmap (fmap (extendFrac . sf (SNat @7))) x
+    (valid1, sample1,  _ready1) = decimateComplex2 (unconcatI coeffsHalfBand2 :: Vec 4 (Vec 8  (SFixed 1 17))) en sample0
+    (valid2, sample2,  _ready2) = decimateComplex2 (unconcatI coeffsHalfBand2 :: Vec 2 (Vec 16 (SFixed 1 17))) valid1 sample1
+    (valid3, sample3a, _ready3) = decimateComplex2 (unconcatI coeffsHalfBand2 :: Vec 1 (Vec 32 (SFixed 1 17))) valid2 sample2
+
     dat
-        = x
-        & fmap (fmap (extendFrac . sf (SNat @7)))
-        & decimateComplex en 
-        & decimateComplex en1
-        & decimateComplex en2
-        & delayEn undefined en3 
-        & phaseDiff en3 
-        & delayEn undefined en3 
-        & cordic en3
+        = sample3a
+        & delayEn undefined valid3 
+        & phaseDiff valid3 
+        & delayEn undefined valid3 
+        & cordic valid3
         & fmap (truncateFrac . renorm)
-        & decimateReal en3
+        & decimateReal valid3
         & decimateReal en4
         & fmap (pack . truncateFrac)
 
-    en1, en2, en3, en4, en5 :: Signal dom Bool
-    (en5 :> en4 :> en3 :> en2 :> en1 :> Nil) = postscanr (.&&.) en $ sequenceA $ unpack <$> cntr
+    en4, en5 :: Signal dom Bool
+    (en5 :> en4 :> Nil) = postscanr (.&&.) valid3 $ sequenceA $ unpack <$> cntr
         where
-        cntr :: Signal dom (BitVector 5)
-        cntr =  count 0 en
+        cntr :: Signal dom (BitVector 2)
+        cntr =  count 0 valid3
 
